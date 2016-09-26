43a444976438089ceccd03ae5271d2b8025fe373
arashicage <arashicage@yeah.net>
2016-09-26 09:29:17 +0800

    no message

--- 01_prep_data.sh
+++ 01_prep_data.sh
@@ -7,7 +7,7 @@ if [ $# -eq 0 ]; then
 	exit
 fi
 
-for i in $(seq 1 123454321)
+for i in $(seq 1 100)
 do
   i_with_prefix_zero="key"$(echo $i |awk '{printf("%09d\n",$0)}')
   redis-cli -p $1 -c hmset "04:"$i_with_prefix_zero h1 1 h2 2 h000001 1 h000002 2 h010001 1 h010002 2 h010003 3 > /dev/null
